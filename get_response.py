3b3b10841c549cf99ff1d90e7838faf689832c6b
namjmnam <namjmnam@naver.com>
2024-01-17 17:30:00 +0900

    Now can get both df's

--- get_response.py
+++ get_response.py
@@ -2,7 +2,42 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
 import time
+
+def process_table_text(table_text):
+    # Remove all unnecessary strings
+    table_text = table_text.replace(" (US$)", "")
+    table_text = table_text.replace("통화명 환율(원) 전일대비 Cross Rate\n", "")
+    processed_rows = []
+
+    for line in table_text.split('\n'):
+        words = line.split()
+
+        if len(words) >= 3:
+            third_column = words[-1]
+            second_column = words[-3]
+            first_column = " ".join(words[:-3])
+            processed_rows.append([first_column, second_column, third_column])
+
+    df = pd.DataFrame(processed_rows, columns=['통화명', '환율(원)', 'Cross Rate(US$)'])
+    return df
+
+def process_first_table_text(table_text):
+    # Remove all unnecessary strings
+    table_text = table_text.replace("통화명 환율(원) 전일대비\n", "")
+    processed_rows = []
+
+    for line in table_text.split('\n'):
+        words = line.split()
+
+        if len(words) >= 3:
+            second_column = words[-2]
+            first_column = " ".join(words[:-2])
+            processed_rows.append([first_column, second_column])
+
+    df = pd.DataFrame(processed_rows, columns=['통화명', '환율(원)'])
+    return df
 
 # Open webpage
 url = "http://www.smbs.biz/ExRate/TodayExRate.jsp?StrSch_Year=2024&StrSch_Month=01&StrSch_Day=09"
@@ -27,16 +62,14 @@ tables_content = []
 for table in tables:
     tables_content.append(table.get_attribute('outerHTML'))
 
-print(len(tables))
-
-# Iterate over the tables and extract their HTML content
-# for table in tables:
-#     print(table.get_attribute('outerHTML'))
-#     input()
-#     print(table.text)
-#     input()
-print(tables[1].text)
+# print(tables[1].get_attribute('outerHTML'))
+first_table_text = tables[0].text
+table_text = tables[1].text
+df = process_first_table_text(first_table_text)
+# df = process_table_text(table_text)
+print(df.to_string(index=False))
 
 # Close after 0.3 seconds
 time.sleep(0.3)
 driver.quit()
+
