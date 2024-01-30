13602ed3c5501e82b8ce8ef266f56bf0c24565e4
lemol <namjmnam@naver.com>
2024-01-31 02:06:05 +0900

    Added stock files

--- stock_page.py
+++ stock_page.py
@@ -1,6 +1,20 @@
 from __main__ import app
-from flask import render_template
+from flask import request, render_template
 
 @app.route('/stock')
 def stock():
     return render_template('stock.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    start_date = request.form.get('startDate')
+    end_date = request.form.get('endDate')
+    primary_code = request.form.get('primaryCode')
+    primary_value = request.form.get('primaryValue')
+    additional_codes = request.form.getlist('additionalCode[]')
+    additional_values = request.form.getlist('additionalValue[]')
+
+    # Process your data here
+    # ...
+
+    return "Data Received"
